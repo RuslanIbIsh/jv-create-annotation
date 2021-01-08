6a11ee1d58fac5df49f3edb39d8df01ca03c74f7
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2021-01-08 14:46:34 +0200

    smole chenge in main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -7,7 +7,7 @@ import core.basesyntax.dao.UserDaoImpl;
 public class Main {
     public static void main(String[] args) {
         ConsoleHandler handler = new ConsoleHandler(new BetDaoImpl(), new UserDaoImpl());
-        System.out.println("Please, enter value and risk for your bet!");
+        System.out.println("Please, enter Name, age, value and risk for your bet!");
         handler.handle();
     }
 }
